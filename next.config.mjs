c2dcf8ac75c684a98f0c899bfb2324653c15c7e5
pabi5057 <pabitra.panda.5057@gmail.com>
2025-05-09 17:10:56 +0530

    add some points for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:["res.cloudinary.com"],
-    }
-};
-
-export default nextConfig;
+    images: {
+      domains: ["res.cloudinary.com"],
+    },
+    redirects: async () => [
+      {
+        source: '/old-route',
+        destination: '/new-route',
+        permanent: true,
+      },
+    ],
+  };
+  
+  export default nextConfig;
+  
